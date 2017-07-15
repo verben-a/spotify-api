69fe30df07b49fff7bb36ef89b18302fb723cfcc
verben-a <alina.verbenchuk@gmail.com>
2017-07-15 22:37:12 +0700

    Edited

--- run.py
+++ run.py
@@ -23,9 +23,16 @@ def artists_get():
 
 	name = request.args.get('name')
 	results = spotify.search(q='artist:' + name, type = 'artist')
-	print(results)
-	data = json.dumps(results)
+	# extratc the URI
+	one_artist = results['artists']['items'][0]['uri']
+	# print(one_artist, '*********')
+	albums = spotify.artist_albums(one_artist)
+	print(albums);
+	data = json.dumps(albums)
 	return Response(data, 200, mimetype="application/json")
+
+
+# @app.route
 
 
 if __name__ =='__main__':
