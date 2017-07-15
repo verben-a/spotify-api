f80a9b683a7f0f938f9df07339e811e96c153bfd
verben-a <alina.verbenchuk@gmail.com>
2017-07-15 23:17:17 +0700

    Second API call

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from flask import Flask, request, Response
 
 app = Flask(__name__)
+client_credentials_manager = SpotifyClientCredentials()
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 
 @app.route("/")
 def index():
@@ -14,25 +17,24 @@ def index():
 
 @app.route("/api/search", methods = ["GET"])
 def artists_get():
-	# scope = 'user-library-read'
-	# token = util.prompt_for_user_token('alinaverbenchuk', scope)
-	# spotify = spotipy.Spotify(auth=token)
-
-	client_credentials_manager = SpotifyClientCredentials()
-	spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
+	
 	name = request.args.get('name')
 	results = spotify.search(q='artist:' + name, type = 'artist')
-	# extratc the URI
+	# extract the URI
 	one_artist = results['artists']['items'][0]['uri']
-	# print(one_artist, '*********')
+	
 	albums = spotify.artist_albums(one_artist)
 	print(albums);
 	data = json.dumps(albums)
 	return Response(data, 200, mimetype="application/json")
 
 
-# @app.route
+@app.route("/api/album_tracks", methods = ["GET"])
+def album_tracks_get():
+	album_id = request.args.get('album_id')
+	tracks = spotify.album_tracks(album_id)
+	data = json.dumps(tracks)
+	return Response(data, 200, mimetype="application/json")
 
 
 if __name__ =='__main__':
