c11ab04f17acd6fd8e75a300a781affc1df2525e
verben-a <alina.verbenchuk@gmail.com>
2017-07-18 22:42:41 +0100

    Decorators2

--- decorators.py
+++ decorators.py
@@ -1,3 +1,19 @@
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+
+
 from datetime import timedelta
 from flask import make_response, request, current_app
 from functools import update_wrapper
